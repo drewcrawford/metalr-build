c19cde03e5937c54264fa457a4f18d3dd83edc00
Drew Crawford <drew@sealedabstract.com>
2022-03-21 17:47:37 -0500

    Update buildkit
    Support compiler flags

--- src/compile.rs
+++ src/compile.rs
@@ -8,7 +8,7 @@ pub struct MetalCompiler;
 impl CompileStep for MetalCompiler {
     const SOURCE_FILE_EXTENSION: &'static str = "metal";
 
-    fn compile_one(path: &Path, intermediate_dir: &Path, configuration: &Configuration, dependency_path: &Path) -> PathBuf {
+    fn compile_one<'a>(path: &Path, intermediate_dir: &Path, configuration: &Configuration, dependency_path: &Path,flags: impl Iterator<Item=&'a String>) -> PathBuf {
         let file_with_extension = path.file_name().unwrap();
         let file_base_name = path.file_stem().unwrap();
         let mut new_name = file_base_name.to_owned();
@@ -30,7 +30,7 @@ impl CompileStep for MetalCompiler {
             Configuration::Release => (),
         };
 
-        let compile_output = compile_1
+        let compile = compile_1
             //-isysroot-isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX12.1.sdk
             .arg("-ffast-math") //Allow aggressive, lossy floating-point optimizations
             //-serialize-diagnostics <file>
@@ -38,7 +38,12 @@ impl CompileStep for MetalCompiler {
             //-index-store-path <file>
             .arg("-MMD") //write a depfile containing user headers
             .args(&["-MT",file_with_extension.to_str().unwrap()]) //Specify name of main file output in depfile
-            .args(&["-MF",dependency_path.to_str().unwrap()]) //Write depfile output from -MMD, -MD, -MM, or -M to <file>
+            .args(&["-MF",dependency_path.to_str().unwrap()]);
+        for flag in flags {
+            compile.arg(flag);
+        }
+        let compile_output = compile
+                //Write depfile output from -MMD, -MD, -MM, or -M to <file>
             .arg(path) //input file
             .stdout(Stdio::piped()).stderr(Stdio::piped())
             .spawn().unwrap().wait_with_output().unwrap();
